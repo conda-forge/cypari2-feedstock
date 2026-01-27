e78ac31169b9703a24e96b3e29c4b2a119e5ca7f
Julian Rüth <julian.rueth@fsfe.org>
2026-01-27 10:10:51 +0100

    Fix meson invocation

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,7 @@ set -ex
 export PARI_SHARE="$PREFIX/share/pari"
 
 $PYTHON -m build --wheel --no-isolation --skip-dependency-check \
-    -Cbuilddir=builddir \
-    -Csetup-args=${MESON_ARGS_REDUCED// / -Csetup-args=} \
+    -Cbuilddir=builddir -Csetup-args=${MESON_ARGS// / -Csetup-args=} \
     || (cat builddir/meson-logs/meson-log.txt && exit 1)
 
 $PYTHON -m pip install --find-links dist cypari2
