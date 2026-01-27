212e0c60c7c27409b82dd5c69634de7a75f90143
Julian Rüth <julian.rueth@fsfe.org>
2026-01-27 14:42:30 +0100

    Pass pari_datadir without environment variables
    since that is not recommended by meson

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ex
 
-export PARI_SHARE="$PREFIX/share/pari"
-
 $PYTHON -m build --wheel --no-isolation --skip-dependency-check \
-    -Cbuilddir=builddir -Csetup-args=${MESON_ARGS// / -Csetup-args=} \
+    -Cbuilddir=builddir \
+    -Csetup-args=-Dpari_datadir="$PREFIX/share/pari" \
+    -Csetup-args=${MESON_ARGS// / -Csetup-args=} \
     || (cat builddir/meson-logs/meson-log.txt && exit 1)
 
 $PYTHON -m pip install --find-links dist cypari2
