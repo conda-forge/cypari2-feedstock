2eb5125bcaf65d180bc75040890ecacf66347ec5
Julian Rüth <julian.rueth@fsfe.org>
2026-01-27 01:41:50 +0100

    Try with build script from matplotlib

---
+++ recipe/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+
+export PARI_SHARE="$PREFIX/share/pari"
+
+$PYTHON -m build --wheel --no-isolation --skip-dependency-check \
+    -Cbuilddir=builddir \
+    -Csetup-args=${MESON_ARGS_REDUCED// / -Csetup-args=} \
+    || (cat builddir/meson-logs/meson-log.txt && exit 1)
+
+$PYTHON -m pip install --find-links dist cypari2
